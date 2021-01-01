df1761cff5c8e54f22929712d8b2d1c17381d252
Ben Brookes <brookesb91@hotmail.com>
2021-01-01 22:08:36 +0000

    :construction: Updates

--- apps/backend/src/app/infrastructure/scraper.ts
+++ apps/backend/src/app/infrastructure/scraper.ts
@@ -4,21 +4,31 @@ import * as $ from 'cheerio';
 export const scrape = async (url: string) => {
   const html = await request(url);
 
-  const getMetaContent = (property: string) => {
-    const el = $(`meta[property="${property}"][content]`, html);
+  const getMetaContent = (selector: string) => {
+    const el = $(`meta[${selector}][content]`, html);
     const content = el.attr('content') || '';
     return content;
   };
 
+  const getTitle = () => {
+    const el = $(`head title`, html);
+    return el.text();
+  };
+
   return {
-    title: getMetaContent('og:title'),
-    description: getMetaContent('og:description'),
-    locale: getMetaContent('og:locale'),
-    type: getMetaContent('og:type'),
+    title: getMetaContent('property="og:title"') || getTitle(),
+    description:
+      getMetaContent('property="og:description"') ||
+      getMetaContent('name="Description"') ||
+      getMetaContent('name="description"'),
+    locale: getMetaContent('property="og:locale"'),
+    type: getMetaContent('property="og:type"'),
     // url: getMetaContent('og:url'),
-    image: getMetaContent('og:image') || getMetaContent('og:image:url'),
-    name: getMetaContent('og:site_name'),
-    publishedAt: getMetaContent('article:published_time'),
-    modifiedAt: getMetaContent('article:modified_time'),
+    image:
+      getMetaContent('property="og:image"') ||
+      getMetaContent('property="og:image:url"'),
+    name: getMetaContent('property="og:site_name"'),
+    publishedAt: getMetaContent('property="article:published_time"'),
+    modifiedAt: getMetaContent('property="article:modified_time"'),
   };
 };
---
+++ todo.md
@@ -0,0 +1,4 @@
+# TODO
+
+- Add error handling for scraper
+- PWA assets

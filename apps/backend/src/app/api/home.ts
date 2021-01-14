2f544a68ffc7b57de4179dec43a028842b74d8ae
Ben Brookes <brookesb91@hotmail.com>
2021-01-14 22:45:51 +0000

    :wrench: Updates

--- apps/backend/src/app/api/home.ts
+++ apps/backend/src/app/api/home.ts
@@ -1,25 +1,40 @@
 import { Request, Response } from 'express';
 
-import { Tag } from '../models/tag';
+import { Tag, TagDocument } from '../models/tag';
 import { Referer } from '../models/referer';
+
+const TAG_LIMIT = 10;
+const ITEM_LIMIT = 12;
 
 export const home = async (req: Request, res: Response) => {
   const query = {};
-  const limit = 10;
   const sort = { createdAt: -1 };
 
-  const tags = await Tag.find(query).sort(sort).limit(limit);
+  let tag: TagDocument = null;
+
+  if (typeof req.query.tag !== 'undefined') {
+    tag = await Tag.findOne({ name: req.query.tag as string });
+
+    if (!tag) {
+      return res.redirect('/');
+    }
+
+    query['meta.tags'] = { $in: [tag.id] };
+  }
+
+  const tags = await Tag.find({}).sort(sort).limit(TAG_LIMIT);
 
   const items = await Referer.find({
     ...query,
     status: 'complete',
   })
     .sort(sort)
-    .limit(limit)
+    .limit(ITEM_LIMIT)
     .populate('meta.tags');
 
   return res.render('home', {
     tags,
+    tag,
     items,
   });
 };
--- apps/backend/src/app/router.ts
+++ apps/backend/src/app/router.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import { embed } from './api/embed';
 import { home } from './api/home';
-import { tag } from './api/tag';
 
 import { referers } from './api/admin/referers';
 import { tags } from './api/admin/tags';
@@ -11,7 +10,6 @@ const router = Router();
 
 router.get('/', home);
 router.get('/embed', embed);
-router.get('/tags/:slug', tag);
 
 router.get('/admin', (req, res) => res.render('admin/index'));
 router.get('/admin/referers', referers);
